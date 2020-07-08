33b01acbfa5957da8fad266c622cb0f77aaace23
Girish Mahajan <girish.mahajan@yahoo.com>
2020-07-08 13:26:19 +0530

    Initial Checkin

---
+++ src/main/java/com/example/demogitpod/DemoGitpodApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogitpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitpodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitpodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demogitpod/DemoGitpodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogitpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitpodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
